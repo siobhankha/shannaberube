b6c75372b9ae3b2685444b38bf0cbe958f44177b
Shanna Berube <shanna@cannopy.com>
2018-11-11 12:57:43 -0600

    added scrolling support

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef, AfterViewInit } from '@angular/core';
+
+enum Direction {
+  Up = 'Up',
+  Down = 'Down',
+}
 
 @Component({
   selector: 'app-about',
@@ -11,9 +16,25 @@ export class AboutComponent implements OnInit {
 
   @Output() scrollToSection = new EventEmitter()
 
-  constructor() { }
+  initialized: boolean
+
+  constructor(
+    private el: ElementRef
+  ) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.initialized = true
+  }
+
+  scrolledToEnd(dir: Direction) {
+    if(this.initialized) {
+      const scrollable = this.el.nativeElement.firstChild
+      const content = this.el.nativeElement.firstChild.firstChild
+      return dir == Direction.Down ? scrollable.scrollTop + scrollable.offsetHeight >= content.scrollHeight : scrollable.scrollTop == 0
+    }
   }
 
   nextSection() {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ComponentRef, ViewChild } from '@angular/core';
+import { LandingComponent } from './landing/landing.component'
+import { AboutComponent } from './about/about.component'
+import { ContactComponent } from './contact/contact.component'
+
+import { fromEvent } from 'rxjs';
+import {
+  distinctUntilChanged,
+  filter,
+  map,
+  pairwise,
+  share,
+  throttleTime
+} from 'rxjs/operators';
+
+enum Direction {
+  Up = 'Up',
+  Down = 'Down',
+}
 
 @Component({
   selector: 'app-root',
@@ -6,10 +24,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild(AboutComponent) aboutEl
 
   activeSection = 0
 
+  scrolling: boolean = false
+
+  scrollPastDisabled: boolean = true
+
+  sectionScrollPaused: boolean = false
+
   scroll(target) {
     this.activeSection = target
+  }
+
+  changeSection(dir: Direction) {
+    if (dir == Direction.Up) {
+      console.log('scrolling up');
+      this.activeSection = this.activeSection > 0 ? this.activeSection - 1 : this.activeSection
+    } else if (dir == Direction.Down) {
+      console.log('scrolling Down');
+      this.activeSection = this.activeSection < 2 ? this.activeSection + 1 : this.activeSection
+    }
+  }
+
+  processEvent(e) {
+    const direction = (<MouseWheelEvent>e).deltaY < 0 ? Direction.Up : Direction.Down
+    if(!this.scrolling) {
+      if(this.activeSection == 1 && this.aboutEl.scrolledToEnd(direction) && !this.sectionScrollPaused) {
+        this.sectionScrollPaused = true
+        setTimeout(() => {
+          this.scrollPastDisabled = false
+          this.sectionScrollPaused = false
+        }, 600)
+      }
+
+      if(this.activeSection != 1 || !this.scrollPastDisabled) {
+        this.scrolling = true
+        setTimeout(() => {
+          this.scrolling = false
+          this.scrollPastDisabled = true
+        }, 750)
+        return direction
+      }
+    }
+  }
+
+
+  ngAfterViewInit() {
+    const scroll$ = fromEvent(window, 'wheel').pipe(
+      throttleTime(50),
+      map((e): Direction => this.processEvent(e)),
+      share()
+    );
+
+    const goingUp$ = scroll$.pipe(
+      filter(direction => direction === Direction.Up)
+    );
+
+    const goingDown$ = scroll$.pipe(
+      filter(direction => direction === Direction.Down)
+    );
+
+    goingUp$.subscribe(() => this.changeSection(Direction.Up));
+    goingDown$.subscribe(() => this.changeSection(Direction.Down));
   }
 }
